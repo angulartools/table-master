d6d7ad36dab040faaa494412f4a3041a5bef44af
danieleespindola-brenntag <daniele.espindola@brenntag-external.com>
2024-11-21 11:14:32 -0300

    mais classes sendo exportadas

--- src/public-api.ts
+++ src/public-api.ts
@@ -5,3 +5,7 @@
 export * from './lib/table-data-source';
 export * from './lib/table-element';
 export * from './lib/table-data.validator.service';
+export * from './lib/default-validator.service';
+export * from './lib/table-element-reactive-forms';
+export * from './lib/table-element-template-driven';
+export * from './lib/table-element.factory';
